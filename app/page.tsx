220284258cae3b4f0cabf8f1384e422bb8d3e947
yassinkr <kerraiyassin@gmail.com>
2024-09-16 12:03:29 +0100

    fixing window

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Map from '@/components/Map'
 const page = () => {
+  if(typeof window === 'undefined') return;
   return (
     <div  className='w-screen h-screen bg-white flex justify-center items-center '>
    <div className='w-[1400px] h-[1000px]'>
